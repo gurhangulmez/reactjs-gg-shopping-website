1603ce10e817823f2f3ab7191c6caa591062f262
Gulmez <Gurhan.Gulmez@unilever.com>
2021-07-01 22:16:54 +0100

    first commit

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jack Sparrow</h1>
+          <span className="subtitle">pants</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jack Sparrow 2</h1>
+          <span className="subtitle">pants 2</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jack Sparrow 3</h1>
+          <span className="subtitle">pants 3</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jack Sparrow 4</h1>
+          <span className="subtitle">pants 4</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">Jack Sparrow 5</h1>
+          <span className="subtitle">pants 5</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
